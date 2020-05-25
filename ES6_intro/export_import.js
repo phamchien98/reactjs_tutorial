c7ddaad397ff10932a80e4a3e0e8870f73958759
phamchien98 <phamchien22598hn@gmail.com>
2020-05-25 16:01:54 +0000

    reactjs

---
+++ ES6_intro/export_import.js
@@ -0,0 +1,12 @@
+const member = {
+    name = "Dương Minh Trí"
+}
+export default member;
+
+import member from './member.js';
+console.log(member()); 
+/* ouput: "Dương Minh Trí" */
+
+/* hoặc */
+import "TênBấtKỳ" from './member.js';
+console.log(TênBấtKỳ()); 
